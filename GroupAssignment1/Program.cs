1e8413584f7249a8c3ca50bef5484039843f4e37
Kunj Patel <pkunjk007@gmail.com>
2025-02-12 21:25:01 -0700

    Brief information about project in the program as well as minor commenting of code

--- GroupAssignment1/Program.cs
+++ GroupAssignment1/Program.cs
@@ -1,11 +1,18 @@
 ﻿using GroupAssignment1;
-
+/*
+ * Assignment:Classes and Inherirtance
+ * Authors: Kunj, Hans, Enzo And Aether
+ * Summary: Creating a project which reads and stores data of different appliances,
+ *          Sorts and Filters Data by brand or by appliance type
+ *          Randomly displays an assigned number of appliances
+ *          Checks out appliances
+ */
 // Create appliances list
-List<Appliance> appliances = ApplianceIO.ReadFromFile();
+List<Appliance> appliances = ApplianceIO.ReadFromFile();//Creating list from the txt file
 int userOption = 0;
 
 
-while (userOption != 5)
+while (userOption != 5)//Looping menu until user chooses save and exit (option 5)
 {
     Console.WriteLine("Welcome to Modern Appliances!" +
     "\nHow May We Assist You?" +
@@ -19,7 +26,7 @@ while (userOption != 5)
     string option = Console.ReadLine();
     Console.WriteLine("");
     userOption = Convert.ToInt32(option);
-    switch (userOption)
+    switch (userOption)//Calls appropriate method from the ModernAppliance and ApplianceIO classes in order to perform required tasks
     {
         case 1:
             ModernAppliance.CheckOut(appliances);
